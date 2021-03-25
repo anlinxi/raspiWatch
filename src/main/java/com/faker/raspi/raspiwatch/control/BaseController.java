a8ba26f7167c6e478ff91aa76faf252f176d1d91
anlinxi <anlinxi@gmail.com>
2021-03-25 16:01:51 +0800

    原始springboot项目上传

---
+++ src/main/java/com/faker/raspi/raspiwatch/control/BaseController.java
@@ -0,0 +1,31 @@
+package com.faker.raspi.raspiwatch.control;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @program: SC_OneKeyToBaoZhang
+ * @description: BaseController
+ * @author: 淡梦如烟
+ * @create: 2020-04-07 16:15
+ */
+@Controller
+public class BaseController {
+
+    /**
+     * 日志对象
+     */
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @GetMapping("/")
+    public String index() {
+        logger.info("访问首页" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        return "redirect:/autoCode/creater/autoCodeCreate.html";
+    }
+}
