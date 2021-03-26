084124123b78e1673664b5564fd2c3d84ec7f83a
anlinxi <anlinxi@gmail.com>
2021-03-26 13:40:44 +0800

    读取cpu信息，集成swagger2注解

--- src/main/java/com/faker/raspi/raspiwatch/util/SystemInfoReader.java
+++ src/main/java/com/faker/raspi/raspiwatch/util/SystemInfoReader.java
@@ -1,9 +1,12 @@
 package com.faker.raspi.raspiwatch.util;
 
+import com.faker.raspi.raspiwatch.model.CpuInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SystemInfoReader {
 
@@ -13,33 +16,144 @@ public class SystemInfoReader {
     private static Logger logger = LoggerFactory.getLogger(SystemInfoReader.class);
 
     /**
+     * 系统换行符
+     */
+    private static String lineSeparator = System.lineSeparator();
+
+    /**
      * 测试的主方法
      *
      * @param args 参数
      */
     public static void main(String[] args) {
-        logger.info(getSimpleText("/sys/class/thermal/thermal_zone0/temp"));
+        lineSeparator = "\n";
+        logger.info("树莓派温度:" + String.valueOf(getTemperature()));
+        logger.info("树莓派CPU信息:" + getCpuInfo());
+    }
+
+    public static CpuInfo getCpuInfo() {
+        /*
+        CPU implementer	: 0x41
+        CPU architecture: 8
+        CPU variant	: 0x0
+        CPU part	: 0xd08
+        CPU revision	: 3
+
+        Hardware	: BCM2835
+        Revision	: c03112
+        Serial		: 1000000055194fb2
+        Model		: Raspberry Pi 4 Model B Rev 1.2
+
+         */
+        List<String> cpuInfoList = getMuliText("/sys/class/thermal/thermal_zone0/temp");
+        String tempStr = "        CPU implementer\t: 0x41\n" +
+                "        CPU architecture: 8\n" +
+                "        CPU variant\t: 0x0\n" +
+                "        CPU part\t: 0xd08\n" +
+                "        CPU revision\t: 3\n" +
+                "        \n" +
+                "        Hardware\t: BCM2835\n" +
+                "        Revision\t: c03112\n" +
+                "        Serial\t\t: 1000000055194fb2\n" +
+                "        Model\t\t: Raspberry Pi 4 Model B Rev 1.2";
+        String[] arr = tempStr.split(lineSeparator);
+        cpuInfoList = new ArrayList<>();
+        for (String line : arr) {
+            if (SystemInfoReader.isNotNull(line)) {
+                cpuInfoList.add(line);
+            }
+        }
+//        logger.info(cpuInfoList.toString());
+        CpuInfo cpuInfo = new CpuInfo();
+        cpuInfo.readInfoFromFile(cpuInfoList);
+        return cpuInfo;
+    }
+
+    /**
+     * 获取树莓派温度
+     *
+     * @return 摄氏度
+     */
+    public static Float getTemperature() {
+        String tempStr = getSimpleText("/sys/class/thermal/thermal_zone0/temp");
+        if (tempStr != null) {
+            Float temp1 = Float.parseFloat(tempStr);
+            //计算出摄氏度
+            Float temp2 = temp1 / 1000;
+            return temp2;
+        }
+        return null;
     }
 
     /**
      * 获取单行文本
      *
-     * @param path
-     * @return
+     * @param path 路径
+     * @return 文本
      */
     public static String getSimpleText(String path) {
         String temp = getText(path);
-        if (null != temp && temp.endsWith(System.lineSeparator())) {
-            temp = temp.substring(0, temp.length() - System.lineSeparator().length());
+        if (null != temp && temp.endsWith(lineSeparator)) {
+            temp = temp.substring(0, temp.length() - lineSeparator.length());
         }
         return temp;
     }
 
     /**
+     * 获取多行文本
+     *
+     * @param path 路径
+     * @return 多行文本列表
+     */
+    public static List<String> getMuliText(String path) {
+        List<String> stringList = null;
+        String temp = getText(path);
+        if (null != temp) {
+            String[] arr = temp.split(lineSeparator);
+            stringList = new ArrayList<>();
+            for (String line : arr) {
+                if (SystemInfoReader.isNotNull(line)) {
+                    stringList.add(line);
+                }
+            }
+        }
+        return stringList;
+    }
+
+    /**
+     * 判断字符串对象是否为空
+     *
+     * @param object 对象
+     * @return 判断结果
+     */
+    public static boolean isNull(Object object) {
+        if (object == null) {
+            return true;
+        } else {
+            String str = String.valueOf(object);
+            if ("".equals(str.trim())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * 判断字符串对象是否不为空
+     *
+     * @param object 对象
+     * @return 判断结果
+     */
+    public static boolean isNotNull(Object object) {
+        return !isNull(object);
+    }
+
+    /**
      * 根据路径获取文件内容
      *
-     * @param path
-     * @return
+     * @param path 路径
+     * @return 文本
      */
     public static String getText(String path) {
         File file = new File(path);
